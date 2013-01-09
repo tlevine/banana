c1135d3abd761584ce5d645c0f194e4f5b4fb97a
Thomas Levine <occurrence@thomaslevine.com>
2013-01-09 13:38:40 -0600

    test

--- banana.hs
+++ banana.hs
@@ -1,6 +1,16 @@
+-- Compute a board given Bananagrams pieces.
+-- This does not get all valid boards, and
+-- not all computed boards will be valid.
+--
+-- But it should still help.
+
 import qualified Data.Map as Map
 import qualified Data.Set as Set
   
+type FirstWord = String
+type AttachedWord = (String, Char, String)
+type Board = (FirstWord, [AttachedWord], [Char])
+
 sample_lengths_dict = Map.fromList [ (7, Set.fromList ["accusal", "obelisk"]), (11, Set.fromList ["inscription", "insecticide"]) ]
 
 -- board :: Map.Map -> String -> [String]
@@ -11,6 +21,9 @@ sample_lengths_dict = Map.fromList [ (7, Set.fromList ["accusal", "obelisk"]), (
 connects :: Set.Set String -> String -> Bool
 connects words_so_far new_word = True
 
+addWord :: Board -> [Char] -> Board
+addWord oldBoard pieces = newBoard
+
 -- Choose word length combinations
 fan :: Int -> [[Int]]
 fan npieces
@@ -20,6 +33,9 @@ fan npieces
       soFar = map (\ list -> list ++ [2]) (fan (npieces - 1 ))
 
 main = do
+  -- This should add ("r", 'o', "und") to the board.
+  addWord ("elephant", [("", 'p', "en"), ("ba", 'n', "ana")], ("p", 'o', "tato"), "rdnu")
+
   putStrLn $ show $ fan 5
   putStrLn $ show $ fan 4
   putStrLn $ show $ fan 3
